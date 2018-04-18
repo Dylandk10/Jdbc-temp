92529386bfd7ed1a892211fa099a2406863a1f82
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2018-04-17 20:02:51 -0400

    added better lookup to mysql

--- src/App.java
+++ src/App.java
@@ -1,19 +1,39 @@
+import java.sql.SQLException;
 import java.util.Scanner;
 
-//need to come back and edit data enter...
+//simple terminal commands till i make GUI
 
 public class App {
 
 	public static void main(String[] args) {
+		String firstName = null;
+		String lastName = null;
+		String email = null;
+		String position = null;
 		// TODO Auto-generated method stub
-		System.out.println("new table? update record?");
-		Scanner userAnswer = new Scanner(System.in);
-		if(userAnswer.equals("table")) {
-			String firstName = null;
-			String lastName = null;
-			String email = null;
-			String position = null;
+		System.out.println("new entry? update record?");
+		Scanner scanner = new Scanner(System.in);
+		String result = scanner.nextLine();
+		if(result.equals("entry")) {
 			System.out.println("Enter firstname");
+			scanner = new Scanner(System.in);
+			String userName = scanner.nextLine();
+			firstName = userName;
+			System.out.println("LastName");
+			scanner = new Scanner(System.in);
+			lastName = scanner.nextLine();
+			
+		} else if(result.equals("lookup")) {
+			System.out.println("enter first Name");
+			Scanner scanner1 = new Scanner(System.in);
+			firstName = scanner1.nextLine();
+			try {
+				LookUp lookUp = new LookUp(firstName);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 
 	}
---
+++ src/LookUp.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LookUp {
+	public LookUp(String firstName) throws SQLException {
+	Connection myConn = null;
+	PreparedStatement myStmt = null;
+	ResultSet myRes = null;
+	
+	String dbUrl = "jdbc:mysql://localhost:3306/demo";
+	String user = "student";
+	String pass = "student";
+	
+	myConn = DriverManager.getConnection(dbUrl, user, pass);
+	
+	myStmt = myConn.prepareStatement("select * from employees where first_name=?");
+	myStmt.setString(1, firstName);
+	myRes = myStmt.executeQuery();
+	display(myRes);
+	
+	if (myRes != null) {
+		myRes.close();
+	}
+	
+	if (myStmt != null) {
+		myStmt.close();
+	}
+	
+	if (myConn != null) {
+		myConn.close();
+	}
+	
+	}
+	public static void display(ResultSet myRs) throws SQLException {
+		while (myRs.next()) {
+			String lastName = myRs.getString("last_name");
+			String firstName = myRs.getString("first_name");
+			double salary = myRs.getDouble("salary");
+			String department = myRs.getString("department");
+			
+			System.out.printf("%s, %s, %.2f, %s\n", lastName, firstName, salary, department);
+		}
+	}
+
+}
